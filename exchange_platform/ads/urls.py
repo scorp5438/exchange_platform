63e3a88ea5ad4e5030962d810469007c71e15a15
Punisher <alex_77_90@mail.ru>
2025-07-06 14:56:11 +0500

    Create filters and search

--- exchange_platform/ads/urls.py
+++ exchange_platform/ads/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import IndexView, DetailAdView, CreateAdView, UpdateAdView
+from .views import IndexView, DetailAdView, CreateAdView, UpdateAdView, DeleteAdView
 
 app_name = 'ads'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('ad/<int:pk>', DetailAdView.as_view(), name='detail_ad'),
     path('create_ad/', CreateAdView.as_view(), name='create_ad'),
     path('update_ad/<int:pk>', UpdateAdView.as_view(), name='update_ad'),
+    path('delete_ad/<int:pk>', DeleteAdView.as_view(), name='delete_ad'),
 ]
--- exchange_platform/ads/views.py
+++ exchange_platform/ads/views.py
@@ -1,13 +1,51 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Q
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from .models import Ad
 from .forms import AdForm
+from .models import Ad, Category
+
 
 class IndexView(ListView):
-    queryset = Ad.objects.select_related('category', 'user').all()
     template_name = 'ads/index.html'
+    paginate_by = 5
+
+    def get_queryset(self):
+        queryset = Ad.objects.select_related('category', 'user').all()
+
+        category_id = self.request.GET.get('category')
+        condition = self.request.GET.get('condition')
+        search_query = self.request.GET.get('search')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query)
+            )
+
+        if category_id and category_id.isdigit():
+            category_id = int(category_id)
+        else:
+            category_id = 0
+
+        if category_id:
+            queryset = queryset.filter(category_id=category_id)
+
+        if condition:
+            queryset = queryset.filter(condition=condition)
+
+        return queryset.order_by('-created_at')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['conditions'] = Ad.CONDITION
+        context['search_query'] = self.request.GET.get('search', '')
+        context['current_category'] = self.request.GET.get('category', '')
+        context['current_condition'] = self.request.GET.get('condition', '')
+        return context
+
 
 class DetailAdView(DetailView):
     queryset = Ad.objects.select_related('category', 'user').all()
@@ -35,6 +73,19 @@ class UpdateAdView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def get_success_url(self):
         return reverse('ads:detail_ad', kwargs={'pk': self.object.pk})
+
+    def test_func(self):
+        if self.request.user.is_superuser:
+            return True
+
+        created_by_current_user = self.get_object().user == self.request.user
+        return created_by_current_user
+
+
+class DeleteAdView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Ad
+    template_name = 'ads/delete_ad.html'
+    success_url = reverse_lazy('ads:index')
 
     def test_func(self):
         if self.request.user.is_superuser:
